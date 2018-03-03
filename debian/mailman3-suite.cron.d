8b3c62ee0fd7c650baa8baf3e83311092be86bdb
Jonas Meurer <jonas@freesources.org>
2018-03-03 10:47:20 +0100

    d/mailman3-suite.cron.d: Run cronjobs as user 'www-data'.

--- debian/mailman3-suite.cron.d
+++ debian/mailman3-suite.cron.d
@@ -1,9 +1,9 @@
 # Cronjobs for the Mailman3 Django project (mailman3-suite)
 
-* *	* * *	list	command -v django-admin >/dev/null && django-admin runjobs minutely --pythonpath /usr/share/mailman3-suite --settings settings
-2,17,32,47 * * * * root	command -v django-admin >/dev/null && django-admin runjobs quarter_hourly --pythonpath /usr/share/mailman3-suite --settings settings
-@hourly		list	command -v django-admin >/dev/null && django-admin runjobs hourly   --pythonpath /usr/share/mailman3-suite --settings settings
-@daily		list	command -v django-admin >/dev/null && django-admin runjobs daily    --pythonpath /usr/share/mailman3-suite --settings settings
-@weekly		list	command -v django-admin >/dev/null && django-admin runjobs weekly   --pythonpath /usr/share/mailman3-suite --settings settings
-@monthly	list	command -v django-admin >/dev/null && django-admin runjobs monthly  --pythonpath /usr/share/mailman3-suite --settings settings
-@yearly		list	command -v django-admin >/dev/null && django-admin runjobs yearly   --pythonpath /usr/share/mailman3-suite --settings settings
+* *	* * *	www-data	command -v django-admin >/dev/null && django-admin runjobs minutely --pythonpath /usr/share/mailman3-suite --settings settings
+2,17,32,47 * * * * www-data	command -v django-admin >/dev/null && django-admin runjobs quarter_hourly --pythonpath /usr/share/mailman3-suite --settings settings
+@hourly		www-data	command -v django-admin >/dev/null && django-admin runjobs hourly   --pythonpath /usr/share/mailman3-suite --settings settings
+@daily		www-data	command -v django-admin >/dev/null && django-admin runjobs daily    --pythonpath /usr/share/mailman3-suite --settings settings
+@weekly		www-data	command -v django-admin >/dev/null && django-admin runjobs weekly   --pythonpath /usr/share/mailman3-suite --settings settings
+@monthly	www-data	command -v django-admin >/dev/null && django-admin runjobs monthly  --pythonpath /usr/share/mailman3-suite --settings settings
+@yearly		www-data	command -v django-admin >/dev/null && django-admin runjobs yearly   --pythonpath /usr/share/mailman3-suite --settings settings
