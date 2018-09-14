4dc86a7d3d00fc1551583325f16ae3f74d9395b4
Jonas Meurer <jonas@freesources.org>
2018-09-14 23:49:48 +0200

    d/mailman3-web.cron.d: Run cronjobs as python3

--- debian/mailman3-web.cron.d
+++ debian/mailman3-web.cron.d
@@ -1,9 +1,9 @@
 # Cronjobs for the Mailman3 Django project (mailman3-web)
 
-* *	* * *	www-data	[ -f /usr/bin/django-admin ] && python2.7 /usr/bin/django-admin runjobs minutely --pythonpath /usr/share/mailman3-web --settings settings
-2,17,32,47 * * * * www-data	[ -f /usr/bin/django-admin ] && python2.7 /usr/bin/django-admin runjobs quarter_hourly --pythonpath /usr/share/mailman3-web --settings settings
-@hourly		www-data	[ -f /usr/bin/django-admin ] && python2.7 /usr/bin/django-admin runjobs hourly   --pythonpath /usr/share/mailman3-web --settings settings
-@daily		www-data	[ -f /usr/bin/django-admin ] && python2.7 /usr/bin/django-admin runjobs daily    --pythonpath /usr/share/mailman3-web --settings settings
-@weekly		www-data	[ -f /usr/bin/django-admin ] && python2.7 /usr/bin/django-admin runjobs weekly   --pythonpath /usr/share/mailman3-web --settings settings
-@monthly	www-data	[ -f /usr/bin/django-admin ] && python2.7 /usr/bin/django-admin runjobs monthly  --pythonpath /usr/share/mailman3-web --settings settings
-@yearly		www-data	[ -f /usr/bin/django-admin ] && python2.7 /usr/bin/django-admin runjobs yearly   --pythonpath /usr/share/mailman3-web --settings settings
+* *	* * *	www-data	[ -f /usr/bin/django-admin ] && python3 /usr/bin/django-admin runjobs minutely --pythonpath /usr/share/mailman3-web --settings settings
+2,17,32,47 * * * * www-data	[ -f /usr/bin/django-admin ] && python3 /usr/bin/django-admin runjobs quarter_hourly --pythonpath /usr/share/mailman3-web --settings settings
+@hourly		www-data	[ -f /usr/bin/django-admin ] && python3 /usr/bin/django-admin runjobs hourly   --pythonpath /usr/share/mailman3-web --settings settings
+@daily		www-data	[ -f /usr/bin/django-admin ] && python3 /usr/bin/django-admin runjobs daily    --pythonpath /usr/share/mailman3-web --settings settings
+@weekly		www-data	[ -f /usr/bin/django-admin ] && python3 /usr/bin/django-admin runjobs weekly   --pythonpath /usr/share/mailman3-web --settings settings
+@monthly	www-data	[ -f /usr/bin/django-admin ] && python3 /usr/bin/django-admin runjobs monthly  --pythonpath /usr/share/mailman3-web --settings settings
+@yearly		www-data	[ -f /usr/bin/django-admin ] && python3 /usr/bin/django-admin runjobs yearly   --pythonpath /usr/share/mailman3-web --settings settings
